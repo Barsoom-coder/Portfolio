55db846f89bc6d2e58ebcc72f8ce26c679527231
roman_kononov <romankononovdev@mail.ru>
2020-12-24 16:33:34 +0700

    added section pageProofs

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,42 @@
 import $ from 'jquery'; window.jQuery = $; window.$ = $; // import jQuery module (npm i -D jquery)
+import slick from 'slick-carousel';
 
 // require('~/app/libs/mmenu/js/jquery.mmenu.all.min.js') // import vendor jQuery plugin example (not module)
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+	$(".slider__row").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: ".slider__button--next",
+    prevArrow: ".slider__button--prev",
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 
 });
