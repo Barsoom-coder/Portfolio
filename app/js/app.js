0fc0e4bdfd546fc56ae3552e9ae1843b7ceb8be4
roman_kononov <romankononovdev@mail.ru>
2020-12-26 21:56:02 +0700

    added fonts and smooth scroll

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,12 @@
-import $ from 'jquery'; window.jQuery = $; window.$ = $; // import jQuery module (npm i -D jquery)
-import slick from 'slick-carousel';
-import validate from 'jquery-validation';
+import $ from "jquery";
+window.jQuery = $;
+window.$ = $; // import jQuery module (npm i -D jquery)
+import slick from "slick-carousel";
+import validate from "jquery-validation";
 // require('~/app/libs/mmenu/js/jquery.mmenu.all.min.js') // import vendor jQuery plugin example (not module)
 
-document.addEventListener('DOMContentLoaded', () => {
-
-	$(".slider__row").slick({
+document.addEventListener("DOMContentLoaded", () => {
+  $(".slider__row").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     dots: false,
@@ -37,9 +38,9 @@ document.addEventListener('DOMContentLoaded', () => {
         },
       },
     ],
-	});
-	
-	$("#myForm").validate({
+  });
+
+  $("#myForm").validate({
     rules: {
       name: {
         required: true,
@@ -81,67 +82,60 @@ document.addEventListener('DOMContentLoaded', () => {
     submitHandler: function (form) {
       ajaxFormSubmit();
     },
-	});
-	
+  });
 
-	  // Функция AJAX запрса на сервер
-		function ajaxFormSubmit() {
-			var string = $("#myForm").serialize(); // Соханяем данные введенные в форму в строку.
-	
-			// Формируем ajax запрос
-			$.ajax({
-				type: "POST", // Тип запроса - POST
-				url: "mail.php", // Куда отправляем запрос
-				data: string, // Какие даные отправляем, в данном случае отправляем переменную string
-	
-				// Функция если все прошло успешно
-				success: function (html) {
-					$("#myForm").slideToggle(800);
-					$("#answer").html(html);
-	
-					// Удаление сообщения от удачной отправке
-					$(".contact-form__success").on("click", function () {
-						$("#myForm")[0].reset();
-						$(".contact-form__success").detach();
-						$("#myForm").slideToggle(800);
-					});
-				},
-			});
-	
-			// Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
-			return false;
-	}
-	
-	// $(".btnScroll").click(function () {
-  //   $("html, body").animate({ scrollTop: 0 }, "slow");
-  //   return false;
-	// });
-	
-	// $(window).scroll(function () {
-	// 	if ($(this).scrollTop() > $(window).height() * 0.2) {
-	// 		// $(".btnScroll").css("display", "flex").fadeIn();
-	// 		$(".btnScroll").fadeIn();
-	// 	} else {
-	// 		// $(".btnScroll").css("display", "none").fadeOut();
-	// 		$(".btnScroll").fadeOut();
-	// 	}
-	// });
+  function ajaxFormSubmit() {
+    var string = $("#myForm").serialize();
+    $.ajax({
+      type: "POST",
+      url: "mail.php",
+      data: string,
 
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > $(window).height() * 0.2) {
-			$('.btnScroll').fadeIn();
-		} else {
-			$('.btnScroll').fadeOut();
-		}
-	});
+      success: function (html) {
+        $("#myForm").slideToggle(800);
+        $("#answer").html(html);
 
+        $(".contact-form__success").on("click", function () {
+          $("#myForm")[0].reset();
+          $(".contact-form__success").detach();
+          $("#myForm").slideToggle(800);
+        });
+      },
+    });
+    return false;
+  }
 
-	$('.btnScroll').click(function () {
-		$('body,html').animate({
-			scrollTop: 0
-		}, "slow");
-		return false;
-	});
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > $(window).height() * 0.2) {
+      $(".btnScroll").fadeIn();
+    } else {
+      $(".btnScroll").fadeOut();
+    }
+  });
 
+  $(".btnScroll").click(function (e) {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      "slow"
+    );
+    return false;
+  });
+
+  $(".scroll").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $(hash).offset().top - ($('ul').height() - 1)  
+          },
+          800,
+        );
+    }
+  });
 
 });
